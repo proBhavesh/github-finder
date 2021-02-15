5271f87c7e98ac010e5fc6cdc53aadedb972068a
Bhavesh <bhaveshjat9950@gmail.com>
2021-02-15 23:18:40 +0530

    Made reponsive and bugs cleared

--- ui.js
+++ ui.js
@@ -11,17 +11,17 @@ class UI {
 			<a href="${user.profile.html_url}" target="_blank">View Profile</a>
 		</div>
 		<div class="p-info">
-			<div class="btns btn1">Followers: ${user.profile.followers}</div>
-			<div class="btns btn2">Following: ${user.profile.following}</div>
-			<div class="btns btn3">Public Repos: ${user.profile.public_repos}</div>
-			<div class="btns btn4">Public Gists: ${user.profile.public_gists}</div>
+			<button><div class="btns btn1">Followers: ${user.profile.followers}</div></button>
+			<button><div class="btns btn2">Following: ${user.profile.following}</div></button>
+			<button><div class="btns btn3">Public Repos: ${user.profile.public_repos}</div></button>
+			<button><div class="btns btn4">Public Gists: ${user.profile.public_gists}</div></button>
 		</div>
 
 		<div class="g-info">
-			<div class="list-items">Company ${user.profile.company}</div>
-			<div class="list-items">Blog/Website ${user.profile.blog}</div>
-			<div class="list-items">Location ${user.profile.location}</div>
-			<div class="list-items">Joined On ${user.profile.created_at}</div>
+			<div class="list-items">Company : ${user.profile.company}</div>
+			<div class="list-items">Blog/Website : ${user.profile.blog}</div>
+			<div class="list-items">Location : ${user.profile.location}</div>
+			<div class="list-items">Joined On : ${user.profile.created_at}</div>
 		</div>
 		`;
 	}
@@ -35,7 +35,7 @@ class UI {
 		`;
 		setTimeout(() => {
 			this.clearAlert();
-		}, 500);
+		}, 3000);
 	}
 
 	clearProfile() {
